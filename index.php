9db8d1ac6c0989baaefa14bc4ba5da45d7c15acf
Пожилой молодой человек <49003989+Delivery-Klad@users.noreply.github.com>
2021-10-01 20:08:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html><head>
+    <title>Index of root</title>
+</head><body><p>lab4:</p>
+<a href="/4/">Index.php</a>
+<p>lab7:</p>
+<a href="/7/index1.php">Index1.php</a>
+<br>
+<a href="/7/index2.php">Index2.php</a>
+<br>
+<a href="/7/index3.php">Index3.php</a>
+<br>
+<a href="/7/index3.php">Index4.php</a>
+<p>lab8:</p>
+<a href="/8/weather.php">weather.php</a>
+<p>lab9:</p>
+<a href="/9/swift.php">swift.php</a>
+<p>lab10:</p>
+<a href="/10/dropdown.php">dropdown.php</a></body></html>
